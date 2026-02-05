c4d19494f75ae57806215f2154d00e4eb20cdedb
jellS215 <144711104+jellS215@users.noreply.github.com>
2026-02-05 16:04:22 +0000

    first commit

---
+++ Unit 5/SpringReview.py
@@ -0,0 +1,39 @@
+print(320)
+print(200)
+print(456)
+1,2,4,8,16,32,64,128,256,512
+
+print(320/2) # 0
+print(160/2) # 0
+print(80/2) # 0
+print(40/2) # 0
+print(20/2) # 0
+print(10/2) # 0
+print(5/2) # 1
+print(2/2) # 0
+print(1/2) # 1
+
+# 101000000
+
+print(200/2) # 0
+print(100/2) # 0
+print(50/2) # 0
+print(25/2) # 1
+print(12/2) # 0
+print(6/2) # 0
+print(3/2) # 1
+print(1/2) # 1
+
+# 11001000
+
+print(456/2) # 0
+print(228/2) # 0
+print(114/2) # 0
+print(57/2) # 1
+print(28/2) # 0
+print(14/2) # 0
+print(7/2) # 1
+print(3/2) # 1
+print(1/2) # 1
+
+# 111001000
